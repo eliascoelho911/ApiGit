347394ef8b0030f9c7bb1b2ed737414aaecbe43b
Elias <elias_coelho911@hotmail.com>
2020-10-09 17:37:21 -0300

    Search screen design

--- app/src/main/java/br/com/eliascoelho911/ui/activity/SearchActivity.kt
+++ app/src/main/java/br/com/eliascoelho911/ui/activity/SearchActivity.kt
@@ -2,11 +2,15 @@ package br.com.eliascoelho911.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBar.DISPLAY_SHOW_CUSTOM
 import br.com.eliascoelho911.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        setSupportActionBar(activity_search_toolbar)
     }
 }
