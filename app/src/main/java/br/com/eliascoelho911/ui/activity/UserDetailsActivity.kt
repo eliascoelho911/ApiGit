1e4078a18ed10642070e37b19b08903148086e0d
Elias <elias_coelho911@hotmail.com>
2020-10-09 20:30:19 -0300

    Completed user details screen design

---
+++ app/src/main/java/br/com/eliascoelho911/ui/activity/UserDetailsActivity.kt
@@ -0,0 +1,12 @@
+package br.com.eliascoelho911.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.eliascoelho911.R
+
+class UserDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_details)
+    }
+}
